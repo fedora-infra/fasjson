a13a7d280021f14deda446f2bff12ca5644e89bf
Leonardo Rossetti <2183506+odra@users.noreply.github.com>
2020-03-08 19:22:56 -0300

    /v1/me implementation

--- src/fasjson/web/apis/v1/resources/me.py
+++ src/fasjson/web/apis/v1/resources/me.py
@@ -1,6 +1,33 @@
-from fasjson.web import errors
+import json
+
+import ldap  #type: ignore
+
+from fasjson.web import errors, response
 from fasjson.web.extensions.flask_ldapconn import get_ldap_conn
 
+
 def me():
-    get_ldap_conn()
-    raise errors.WebApiError('method not implemented', 405, data={'reason': 'method not implemented'})
+    try:
+        conn = get_ldap_conn()
+        res = conn.whoami()
+    except ldap.LOCAL_ERROR as e:
+        raise errors.WebApiError('LDAP local error', 500, data={'exception': str(e)})
+    output = {
+        'result': {
+            'raw': res,
+            'info': parse(res)
+        }
+    }
+    return response.ApiResponse(json.dumps(output, sort_keys=True, indent=4), 200)
+
+
+def parse(s):
+    data = {}
+    parts = s.split('dn: ')[1].split(',')
+    data['krbprincipalname'] = parts[0].split('=')[1]
+    for part in parts[1:]:
+        k, v = part.split('=')
+        if not k in data:
+            data[k] = []
+        data[k].append(v)
+    return data
--- test/unit/test_web_resource_v1_me.py
+++ test/unit/test_web_resource_v1_me.py
@@ -3,13 +3,41 @@ import types
 from unittest import mock
 
 import pytest
+import gssapi
+import ldap
 
 from src.fasjson.web import app
 
 
 def test_me_success(client, gss_env):
-    pytest.skip('TODO: implementation missing')
+    r = 'dn: krbprincipalname=http/fasjson.example.test@example.test,cn=services,cn=accounts,dc=example,dc=test'
+    with mock.patch('gssapi.Credentials') as G, mock.patch('ldap.initialize') as L:
+        G.return_value = types.SimpleNamespace(lifetime=10)
+        L.return_value = types.SimpleNamespace(
+            sasl_interactive_bind_s=lambda s, n: '',
+            whoami_s=lambda: r)
+        rv = client.get('/v1/me', environ_base=gss_env)
+    expected = {
+        'result': {
+            'raw': r,
+            'info': {
+                'krbprincipalname': 'http/fasjson.example.test@example.test',
+                'cn': ['services', 'accounts'],
+                'dc': ['example', 'test']
+            }
+        }
+    }
+
+    assert 200 == rv.status_code
+    assert expected == json.loads(rv.data)
 
 
-def test_me_error():
-    pytest.skip('TODO: implementation missing')
+def test_me_error(client, gss_env):
+    with mock.patch('gssapi.Credentials') as G:
+        G.return_value = types.SimpleNamespace(lifetime=10)
+        rv = client.get('/v1/me', environ_base=gss_env)
+    res = json.loads(rv.data)
+    
+    assert 500 == rv.status_code
+    assert 'LDAP local error' == res['error']['message']
+    assert 'Unspecified GSS failure' in res['error']['data']['exception']
