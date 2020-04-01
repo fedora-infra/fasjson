ed1b89570be4f7b80be697c65e0248b5be72f100
Aurélien Bompard <aurelien@bompard.org>
2020-04-02 00:13:50 +0200

    Convert dicts returned by views to JSON
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/web/response.py
+++ fasjson/web/response.py
@@ -1,6 +1,4 @@
-import json
-
-from flask import Response
+from flask import Response, jsonify
 
 from . import errors
 
@@ -9,3 +7,9 @@ class ApiResponse(Response):
     charset = 'utf-8'
     default_status = 200
     default_mimetype = 'application/json'
+
+    @classmethod
+    def force_type(cls, rv, environ=None):
+        if isinstance(rv, dict):
+            rv = jsonify(rv)
+        return super().force_type(rv, environ)
