649af42e0e9d997befb60bdf1cdfe29bdf551c8c
Aurélien Bompard <aurelien@bompard.org>
2020-04-28 11:01:25 +0200

    Allow some namespaces to not be authenticated
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/web/extensions/flask_gss.py
+++ fasjson/web/extensions/flask_gss.py
@@ -1,12 +1,11 @@
-import gssapi  # type: ignore
-from flask import request
 import os
 
-from fasjson.web import errors
-from flask import g
+import gssapi
+from flask import request, g
+from flask_restx import abort
 
 
-class FlaskGSSAPI(object):
+class FlaskGSSAPI:
     def __init__(self, app):
         if app is not None:
             self.init_app(app)
@@ -18,28 +17,19 @@ class FlaskGSSAPI(object):
         g.gss_name = None
         environ = request.environ
         if environ["wsgi.multithread"]:
-            raise errors.WebApiError(
+            abort(
+                500,
                 "GSSAPI is not compatible with multi-threaded WSGI servers.",
-                400,
-                data={"request.environ.name": "wsgi.multithread"},
             )
         ccache = environ.get("KRB5CCNAME")
         if not ccache:
-            raise errors.WebApiError(
-                "KRB5CCNAME missing",
-                500,
-                data={"request.environ.KRB5CCNAME": "KRB5CCNAME"},
-            )
+            return  # Maybe the endpoint is not protected, stop here
         # The LDAP library will look for the cache in the process' environment variables
         os.environ["KRB5CCNAME"] = ccache
 
         principal = environ.get("GSS_NAME")
         if not principal:
-            raise errors.WebApiError(
-                "GSS_NAME missing",
-                500,
-                data={"request.environ.name": "GSS_NAME"},
-            )
+            return  # Maybe the endpoint is not protected, stop here
 
         gss_name = gssapi.Name(principal, gssapi.NameType.kerberos_principal)
         try:
@@ -47,22 +37,17 @@ class FlaskGSSAPI(object):
                 usage="initiate", name=gss_name, store={"ccache": ccache}
             )
         except gssapi.exceptions.GSSError as e:
-            raise errors.WebApiError(
-                "Invalid credentials",
+            abort(
                 403,
-                data={
-                    "codes": {
-                        "maj": e.maj_code,
-                        "min": e.min_code,
-                        "routine": e.routine_code,
-                        "supplementary": e.supplementary_code,
-                    }
+                "Invalid credentials",
+                codes={
+                    "maj": e.maj_code,
+                    "min": e.min_code,
+                    "routine": e.routine_code,
+                    "supplementary": e.supplementary_code,
                 },
             )
-        else:
-            if creds.lifetime <= 0:
-                raise errors.WebApiError(
-                    "Credential lifetime has expired", 401
-                )
+        if creds.lifetime <= 0:
+            abort(401, "Credential lifetime has expired")
 
         g.gss_name = gss_name.display_as(gssapi.NameType.kerberos_principal)
