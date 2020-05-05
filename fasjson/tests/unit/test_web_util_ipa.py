180d9cc38dec1877cf16b05d80b3e6ff75ec449b
Aurélien Bompard <aurelien@bompard.org>
2020-05-05 11:01:17 +0200

    Add endpoints for certificates and CSRs
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/tests/unit/test_web_util_ipa.py
+++ fasjson/tests/unit/test_web_util_ipa.py
@@ -2,7 +2,7 @@ import pytest
 from flask import g
 from werkzeug.exceptions import Unauthorized
 
-from fasjson.web.utils.ipa import ldap_client
+from fasjson.web.utils.ipa import ldap_client, rpc_client
 
 
 def test_ldap_client(mocker, gss_user, app):
@@ -27,3 +27,27 @@ def test_ldap_client_anon(mocker, gss_user, app):
         with pytest.raises(Unauthorized):
             ldap_client()
     get_client.assert_not_called()
+
+
+def test_rpc_client(mocker, gss_user, app):
+    ClientMeta = mocker.patch("fasjson.web.utils.ipa.ClientMeta")
+    client = mocker.Mock()
+    ClientMeta.return_value = client
+    with app.test_request_context("/v1/me/"):
+        app.preprocess_request()
+        g.username = "dummy"
+        rpc_client()
+    ClientMeta.assert_called_once_with(
+        "ipa.example.test", verify_ssl=app.config["FASJSON_IPA_CA_CERT_PATH"]
+    )
+    client.login_kerberos.assert_called_once_with()
+
+
+def test_rpc_client_anon(mocker, gss_user, app):
+    ClientMeta = mocker.patch("fasjson.web.utils.ipa.ClientMeta")
+    with app.test_request_context("/v1/me/"):
+        app.preprocess_request()
+        g.username = None
+        with pytest.raises(Unauthorized):
+            rpc_client()
+    ClientMeta.assert_not_called()
