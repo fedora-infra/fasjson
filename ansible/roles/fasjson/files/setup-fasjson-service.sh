0286c0ae5d0e2ddfa24fc635ef2e8317bd63bc38
Aurélien Bompard <aurelien@bompard.org>
2020-05-05 10:42:26 +0200

    Allow FASJSON to connect to IPA's API
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- ansible/roles/fasjson/files/setup-fasjson-service.sh
+++ ansible/roles/fasjson/files/setup-fasjson-service.sh
@@ -7,11 +7,28 @@ DELEGATION="fasjson-delegation"
 
 ipa service-find $PRINCIPAL &> /dev/null || ipa service-add $PRINCIPAL --force
 
+# Create delegation rule
+
 ipa servicedelegationrule-find $DELEGATION &> /dev/null || ipa servicedelegationrule-add $DELEGATION
 
 ipa servicedelegationrule-show $DELEGATION | grep "Member principals:" | grep -qs $PRINCIPAL || (
 	ipa servicedelegationrule-add-member --principals=$PRINCIPAL $DELEGATION
 )
+
+# Delegate for LDAP
+
 ipa servicedelegationrule-show $DELEGATION | grep "Allowed Target:" | grep -qs ipa-ldap-delegation-targets || (
 	ipa servicedelegationrule-add-target --servicedelegationtargets=ipa-ldap-delegation-targets $DELEGATION
+)
+
+# Delegate for HTTP
+
+ipa servicedelegationtarget-find ipa-http-delegation-targets &> /dev/null || ipa servicedelegationtarget-add ipa-http-delegation-targets
+
+ipa servicedelegationtarget-show ipa-http-delegation-targets | grep "Member principals:" | grep -qs HTTP/ipa.example.test || (
+	ipa servicedelegationtarget-add-member ipa-http-delegation-targets --principals=HTTP/ipa.example.test@EXAMPLE.TEST
+)
+
+ipa servicedelegationrule-show $DELEGATION | grep "Allowed Target:" | grep -qs ipa-http-delegation-targets || (
+	ipa servicedelegationrule-add-target --servicedelegationtargets=ipa-http-delegation-targets $DELEGATION
 )
