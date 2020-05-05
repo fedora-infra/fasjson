85b39f8eb4176df78227699e8fd240c422d10ec8
Aurélien Bompard <aurelien@bompard.org>
2020-05-05 11:01:47 +0200

    Handle RPC API errors
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/tests/unit/test_web_namespace.py
+++ fasjson/tests/unit/test_web_namespace.py
@@ -1,6 +1,7 @@
 import json
 
 import ldap
+from python_freeipa.exceptions import BadRequest
 
 
 def test_schema(client, gss_user):
@@ -21,6 +22,7 @@ def test_ldap_local_error(client, gss_user, mocker):
     assert rv.status_code == 500
     body = json.loads(rv.data)
     assert body["message"] == "LDAP local error"
+    assert body["source"] == "LDAP"
 
 
 def test_ldap_server_error(client, gss_user, mocker):
@@ -31,3 +33,17 @@ def test_ldap_server_error(client, gss_user, mocker):
     assert rv.status_code == 500
     body = json.loads(rv.data)
     assert body["message"] == "LDAP server is down"
+    assert body["source"] == "LDAP"
+
+
+def test_rpc_bad_request(client, gss_user, mocker):
+    mocker.patch(
+        "fasjson.web.resources.certs.rpc_client",
+        side_effect=BadRequest(message="dummy message", code=42),
+    )
+    rv = client.get("/v1/certs/1/")
+    assert rv.status_code == 400
+    body = json.loads(rv.data)
+    assert body["message"] == "dummy message"
+    assert body["code"] == 42
+    assert body["source"] == "RPC"
