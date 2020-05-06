6b40cf9fa3f09dad0789dbed1df6708cc481d11c
Aurélien Bompard <aurelien@bompard.org>
2020-05-06 10:09:10 +0200

    Add the userCertificate attribute to the User model
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/tests/unit/test_lib_ldap_converters.py
+++ fasjson/tests/unit/test_lib_ldap_converters.py
@@ -12,3 +12,9 @@ def test_bool_wrong_value():
     c = converters.BoolConverter("locked")
     with pytest.raises(ValueError):
         assert c.from_ldap([b"maybe"])
+
+
+def test_binary():
+    c = converters.BinaryConverter("userCertificate")
+    assert c.ldap_name == "userCertificate;binary"
+    assert c.from_ldap([b"dummy"]) == "ZHVtbXk="
