d91d4701389a9c10f0a82c4b1b6b68c12d3aab1c
Aurélien Bompard <aurelien@bompard.org>
2020-05-21 11:58:04 +0200

    Try to fix the build in ReadTheDocs
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/make-requirements.sh
+++ docs/make-requirements.sh
@@ -9,8 +9,8 @@ set -x
 
 poetry export -f requirements.txt --without-hashes -o "${output}"
 
-# Remove the GSSAPI module because ReadTheDocs does not install C-based modules
-sed -i -e '/^gssapi==/d' "${output}"
+# Remove some modules because ReadTheDocs does not install C-based modules
+sed -i -e '/^gssapi==/d ; /^python-ldap==/d' "${output}"
 
 # Add toml to parse the version in conf.py
 echo toml >> "${output}"
