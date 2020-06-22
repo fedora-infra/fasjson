b82b953cdf609c3acee19469a70689269830885c
Stephen Coady <scoady@redhat.com>
2020-06-22 16:30:51 +0100

    add non-zero search size check. refactored tests slightly.
    Signed-off-by: Stephen Coady <scoady@redhat.com>

--- fasjson/tests/unit/test_web_resource_v1_search.py
+++ fasjson/tests/unit/test_web_resource_v1_search.py
@@ -11,11 +11,13 @@ def mock_ldap_client(mock_ipa_client):
     yield partial(mock_ipa_client, "fasjson.web.resources.search", "ldap")
 
 
-@pytest.fixture
-def client_with_search_result(mock_ldap_client):
-    data = [_get_user_ldap_data(f"dummy-{idx}") for idx in range(1, 10)]
-    result = LDAPResult(items=data)
-    yield mock_ldap_client(search_users=lambda *a, **kw: result)
+@pytest.fixture()
+def ldap_with_search_result(mock_ldap_client, gss_user):
+    def mock(num_of_results):
+        data = [_get_user_ldap_data(f"dummy-{idx + 1}") for idx in range(0, num_of_results)]
+        result = LDAPResult(items=data)
+        return mock_ldap_client(search_users=lambda *a, **kw: result)
+    return mock
 
 
 def _get_user_ldap_data(name):
@@ -39,7 +41,8 @@ def _get_user_api_output(name):
     return data
 
 
-def test_search_user_success(client, gss_user, client_with_search_result):
+def test_search_user_success(client, ldap_with_search_result):
+    ldap_with_search_result(9)
     rv = client.get("/v1/search/users/?username=dummy")
 
     expected = [_get_user_api_output(f"dummy-{idx}") for idx in range(1, 10)]
@@ -47,10 +50,8 @@ def test_search_user_success(client, gss_user, client_with_search_result):
     assert rv.get_json() == {"result": expected}
 
 
-def test_search_user_not_found(client, gss_user, mock_ldap_client):
-    result = LDAPResult(items=[])
-    mock_ldap_client(search_users=lambda *a, **kw: result)
-
+def test_search_user_not_found(client, ldap_with_search_result):
+    ldap_with_search_result(0)
     rv = client.get("/v1/search/users/?username=somethingobscure")
 
     assert 200 == rv.status_code
@@ -62,7 +63,7 @@ def test_search_user_no_args(client, gss_user, mock_ldap_client):
 
     rv = client.get("/v1/search/users/")
 
-    expected = {"message": "At least one search term must be provided"}
+    expected = {"message": "At least one search term must be provided."}
     assert 400 == rv.status_code
     assert expected == rv.get_json()
 
@@ -81,7 +82,7 @@ def test_search_user_short_search_term(client, gss_user, mock_ldap_client):
     assert expected == rv.get_json()
 
 
-def test_search_user_page_size(client, gss_user, mock_ldap_client):
+def test_search_user_page_size_too_big(client, gss_user, mock_ldap_client):
     mock_ldap_client(search_users=lambda *a, **kw: None)
 
     rv = client.get(
@@ -90,7 +91,22 @@ def test_search_user_page_size(client, gss_user, mock_ldap_client):
 
     expected = {
         "page_size": 123,
-        "message": "Page size cannot be greater than 40 when searching.",
+        "message": "Page size must be between 1 and 40 when searching.",
+    }
+    assert 400 == rv.status_code
+    assert expected == rv.get_json()
+
+
+def test_search_user_page_size_zero(client, gss_user, mock_ldap_client):
+    mock_ldap_client(search_users=lambda *a, **kw: None)
+
+    rv = client.get(
+        "/v1/search/users/?username=somethinginsignificant&page_size=0"
+    )
+
+    expected = {
+        "page_size": 0,
+        "message": "Page size must be between 1 and 40 when searching.",
     }
     assert 400 == rv.status_code
     assert expected == rv.get_json()
