278a637a78feb79eb0b6344d11e4b50c4fe6e568
Aurélien Bompard <aurelien@bompard.org>
2020-09-17 16:19:04 +0200

    Don't try to reuse LDAP clients
    This is probably the cause of errors such as:
    ```
    ldap.SERVER_DOWN: {'result': -1, 'desc': "Can't contact LDAP server", 'errno': 32, 'ctrls': [], 'info': 'Broken pipe'}
    ```
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fasjson/lib/ldap/__init__.py
+++ fasjson/lib/ldap/__init__.py
@@ -1,12 +1,5 @@
 from .client import LDAP
 
 
-# Re-use connections
-__ldap = {}
-
-
 def get_client(uri, basedn, login, **kwargs):
-    global __ldap
-    if login not in __ldap:
-        __ldap[login] = LDAP(uri, basedn, **kwargs)
-    return __ldap[login]
+    return LDAP(uri, basedn, **kwargs)
