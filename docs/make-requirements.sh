eb4200374c0300d026f21b262fa9844d3c30be3e
Aurélien Bompard <aurelien@bompard.org>
2021-05-12 10:22:54 +0200

    Fix readthedocs deps
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/make-requirements.sh
+++ docs/make-requirements.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-here=`dirname $0`
+here=$(realpath $(dirname $0))
 output="${here}/requirements.txt"
+excluded="gssapi python-ldap requests-kerberos pykerberos winkerberos flask-mod-auth-gssapi"
 
 set -x
 
 poetry export -f requirements.txt --without-hashes -o "${output}"
 
 # Remove some modules because ReadTheDocs does not install C-based modules
-for exclude in gssapi python-ldap requests-kerberos pykerberos winkerberos; do
+for exclude in ${excluded}; do
     sed -i -e "/^${exclude}==/d" "${output}"
 done
 
