eb5bd2f099addf5e664d60244d74db68000d0608
Aurélien Bompard <aurelien@bompard.org>
2021-11-04 18:44:40 +0100

    Update the docs requirements
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/make-requirements.sh
+++ docs/make-requirements.sh
@@ -10,6 +10,9 @@ set -x
 
 poetry export -f requirements.txt --without-hashes -o "${output}"
 
+# Remove the python version markers
+sed -i -e "s/; .*$//" "${output}"
+
 # Remove some modules because ReadTheDocs does not install C-based modules
 for exclude in ${excluded}; do
     sed -i -e "/^${exclude}==/d" "${output}"
