f18855352291f69c73b60f7643c2702b357a1990
Aurélien Bompard <aurelien@bompard.org>
2023-06-06 16:16:53 +0200

    Fix the licenses check
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -6,11 +6,6 @@ set -e
 
 TMPFILE=$(mktemp -t noggin-requirements-XXXXXX.txt)
 
-# Note: we can't use poetry export because it isn't smart enough with conditional dependencies:
-# flake8 requires importlib_metadata on python < 3.8, so it's not installed, but it's exported
-# and liccheck crashes on packages listed in the req file but not installed.
-# poetry export --dev -f requirements.txt -o $TMPFILE
-
-poetry run pip freeze --exclude-editable --isolated > $TMPFILE
+poetry export --with dev -f requirements.txt --without-hashes -o $TMPFILE
 
 poetry run liccheck -r $TMPFILE
