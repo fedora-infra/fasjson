f820ca523d1ab1abe0727a692c5984752d93a31e
Aurélien Bompard <aurelien@bompard.org>
2025-11-17 09:59:09 +0100

    Add setuptools when running liccheck
    until a better solution is found upstream.
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -22,4 +22,7 @@ poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE
 # Use pip freeze instead of poetry when it fails
 # poetry run pip freeze --exclude-editable --isolated > $TMPFILE
 
+# Liccheck requires setuptools: https://github.com/dhatim/python-license-check/issues/117
+poetry run pip install setuptools
+
 poetry run liccheck -r $TMPFILE -s $STRATEGY_TMPFILE
