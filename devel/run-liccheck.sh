604d3b03693d3b091ec504267f393cbf60c60dbf
Aurélien Bompard <aurelien@bompard.org>
2026-02-16 18:06:22 +0100

    Fix version requirement for liccheck
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -22,7 +22,7 @@ poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE
 # Use pip freeze instead of poetry when it fails
 # poetry run pip freeze --exclude-editable --isolated > $TMPFILE
 
-# Liccheck requires setuptools: https://github.com/dhatim/python-license-check/issues/117
-poetry run pip install setuptools
+# Liccheck requires pkg_resources: https://github.com/dhatim/python-license-check/issues/117
+poetry run pip install "setuptools<82.0.0"
 
 poetry run liccheck -r $TMPFILE -s $STRATEGY_TMPFILE
